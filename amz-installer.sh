5a4ed6dbb336d0fc01cdc1b0e84fb5b9c04c1e8d
Tom Mairs <tom.mairs@messagesystems.com>
2021-03-02 08:26:22 -0700

    Update amz-installer.sh

--- amz-installer.sh
+++ amz-installer.sh
@@ -6,7 +6,7 @@
 ##################################################################################
 
 # First build an Amazon Linux AMI and copy this installer script to /tmp
-# This installer uses Amazon Linux AMI release 2017.09
+# This installer uses Amazon Linux AMI  amzn2-ami-hvm-2.0.20210219.0-x86_64-gp2
 
 # You should just execute this as root, but if you copy and paste this, make sure you sudo first.
 #sudo -s
@@ -21,25 +21,6 @@ sed -i "s/HOSTNAME=.*/HOSTNAME=$FQDN/" /etc/sysconfig/network
 echo  "$PRIVATEIP    $FQDN" >> /etc/hosts
 hostname $FQDN
 
-echo "
-*filter
-:INPUT ACCEPT [0:0]
-:FORWARD ACCEPT [0:0]
-:OUTPUT ACCEPT [82:15000]
--A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
--A INPUT -p icmp -j ACCEPT
--A INPUT -i lo -j ACCEPT
--A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
--A INPUT -i eth0 -p tcp -m tcp --dport 25 -j ACCEPT
--A INPUT -i eth0 -p tcp -m tcp --dport 80 -j ACCEPT
--A INPUT -i eth0 -p tcp -m tcp --dport 81 -j ACCEPT
--A INPUT -i eth0 -p tcp -m tcp --dport 443 -j ACCEPT
--A INPUT -i eth0 -p tcp -m tcp --dport 587 -j ACCEPT
--A INPUT -j REJECT --reject-with icmp-host-prohibited
--A FORWARD -j REJECT --reject-with icmp-host-prohibited
-COMMIT
-" > /etc/sysconfig/iptables
-service iptables restart
 
 
 echo "
@@ -57,8 +38,8 @@ SELINUXTYPE=targeted
 
 /usr/sbin/setenforce 0
 
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
+#rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#rpm -Uvh https://mirror.webtatic.com/yum/el7/epel-release.rpm
 
 yum clean headers
 yum clean packages
@@ -66,28 +47,75 @@ yum clean metadata
 
 yum update -y
 
+# Install any supporting packages that we may want for later...
+yum -y install perl mcelog sysstat ntp gdb lsof.x86_64 wget yum-utils bind-utils telnet mlocate lynx unzip sudo
+yum -y install make gcc curl cpan firewalld lynx
 yum -y install perl sysstat ntp gdb lsof.x86_64 wget yum-utils bind-utils telnet mlocate lynx unzip sudo 
-yum -y install lynx php-devel php-gd php-imap php-ldap php-mysql php-odbc php-xml php-xmlrpc php-pgsql  
-yum -y install httpd mysql mysql-devel mysql-server which flex make gcc wget unzip zip nmap fileutils gcc-c++ curl curl-devel 
+yum -y install httpd httpd httpd-tools mod_ssl mysql mysql-devel mysql-server 
+yum -y install which flex make gcc wget unzip zip nmap fileutils gcc-c++ curl curl-devel 
 yum -y install perl-libwww-perl ImageMagick libxml2 libxml2-devel perl-HTML-Parser perl-DBI perl-Net-DNS perl-URI perl-Digest-SHA1 
 yum -y install postgresql postgresql-contrib postgresql-devel postgresql-server cpan perl-YAML mod_ssl openssl
 
-# Uncomment this section if you want PHP 5.6
-#yum -y remove php*
-#yum -y install php56w php56w-opcache
-#yum -y install php56w* --skip-broken
-#yum -y remove php56w-mysqlnd
-#yum -y install php56w-mysql
+wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+yum localinstall -y mysql57-community-release-el7-11.noarch.rpm 
+yum install -y mysql-community-server
+systemctl start mysqld.service
 
-# Uncomment this section if you want PHP 7.0
+
+# Installing PHP 7.3
 yum -y remove php*
-yum -y install php70w php70w-opcache
-yum -y install php70w* --skip-broken
-yum -y remove php70w-mysqlnd
-yum -y install php70w-mysql
+amazon-linux-extras disable php7.4
+amazon-linux-extras disable php7.2
+amazon-linux-extras enable php7.3
+yum install -y php php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
+
+#Make sure it all stays up to date
+#Run a yum update at 3AM daily
+echo "0 3 * * * root /usr/bin/yum update -y >/dev/null 2>&1">/etc/cron.d/yum-updates
+
 
 # Make mlocatedb current
 updatedb
+
+
+
+# Turn off any services that we do not want
+# Turn on any ones that we do need
+systemctl stop iptables.service
+systemctl stop ip6tables.service
+systemctl disable iptables.service
+systemctl disable ip6tables.service
+
+systemctl enable ntpd.service
+systemctl start  ntpd.service
+
+systemctl stop  postfix.service
+systemctl disable postfix.service
+
+systemctl stop  qpidd.service
+systemctl disable qpidd.service5
+
+# Update and start Firewalld
+systemctl enable firewalld
+
+echo "ZONE=public
+" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+
+systemctl stop firewalld
+systemctl start firewalld.service
+firewall-cmd --set-default-zone=public
+firewall-cmd --zone=public --change-interface=eth0
+firewall-cmd --zone=public --permanent --add-service=http
+firewall-cmd --zone=public --permanent --add-service=https
+firewall-cmd --zone=public --permanent --add-service=ssh
+firewall-cmd --zone=public --permanent --add-service=smtp
+firewall-cmd --zone=public --permanent --add-port=587/tcp
+firewall-cmd --zone=public --permanent --add-port=81/tcp
+firewall-cmd --zone=public --permanent --add-port=2081/tcp
+firewall-cmd --zone=public --permanent --add-port=2084/tcp
+
+systemctl enable firewalld
+
 
 chkconfig postgresql on
 service postgresql initdb
@@ -107,13 +135,10 @@ source /etc/bashrc
 
 # Install Node
 cd /usr/src
-curl --silent --location https://rpm.nodesource.com/setup_9.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum -y install nodejs
 
 cd /tmp
-
-export LANG=en_US
-/usr/bin/cpan CPAN LWP::UserAgent Carp URI JSON Data::Dumper XML::Simple DBI DBD::ODBC JSON::PP::Boolean MAKAMAKA/JSON-2.51.tar.gz JSON --force
 
 # Generate private key 
 openssl genrsa -out ca.key 2048 
@@ -138,70 +163,52 @@ chkconfig --level 345 mysqld on
 chkconfig --level 345 httpd on
 service httpd restart
 service mysqld restart
-/usr/bin/mysql_secure_installation
 
-#PAUSE
 
 chkconfig ntpd on 
 /etc/init.d/ntpd start 
 service postfix stop
 /sbin/chkconfig postfix off
 
-# Create a separate "ops" user with sudo access:
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config   
-echo "ops       ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-service sshd restart
-
-useradd ops
-passwd ops
-
 echo
-
 echo
-
 echo
-
 echo
-
 echo
-
 echo
-
 echo
-
 echo
 
 echo "NODE Version:"
 node --version
 echo
-echo
 echo "PHP Version:"
 php --version
-echo
 echo
 echo "PERL Version:"
 perl --version
 echo
-echo
 echo "MySQL Version:"
 mysql --version
-echo
 echo
 echo "Apache HTTPD Version:"
 httpd -v
 echo
-echo
 echo "OpenSSL Version:"
 openssl version
-echo
 echo
 echo "PostgreSQL Version:"
 /usr/bin/psql --version
 echo
+echo "Git Version:"
+/usr/bin/git --version
 echo
 
+echo "Now running ' /usr/bin/mysql_secure_installation ' "
+echo "Remember to use this temporary root password:"
+grep 'temporary password' /var/log/mysqld.log
 
-echo "You should 'init 6' here to make sure everything comes back up ok."
+echo "You should then 'init 6' here to make sure everything comes back up ok."
 
-echo "DONE"
+/usr/bin/mysql_secure_installation
 
